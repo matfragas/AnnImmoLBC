9261acaa5372934265315412df04fa27a051b5f8
matfragas <franck.gasseau@gmail.com>
2025-07-04 17:33:29 +0200

    Update scraper_lbc_html.py

--- scraper_lbc_html.py
+++ scraper_lbc_html.py
@@ -7,11 +7,7 @@ async def main():
     url = (
         "https://www.leboncoin.fr/recherche"
         "?category=9"
-        "&locations=Louverné_53950__48.12273_-0.72003_5000,"
-        "L'Huisserie_53970__48.02281_-0.77001_5000,"
-        "Saint-Berthevin_53940__48.06967_-0.83152_5000,"
-        "Chang%C3%A9_53810__48.09901_-0.78975_5000,"
-        "Laval_53000__48.07268_-0.77307_5000"
+        "&locations=Laval_53000__48.07268_-0.77307_5000"
         "&price=min-320000"
         "&square=85-max"
         "&real_estate_type=1,3"
